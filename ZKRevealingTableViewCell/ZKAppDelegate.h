07164a8845060ab22712aba218f68fb0e9547532
Alex Zielenski <alex@zielenski.net>
2012-04-29 12:03:12 -0400

    Initial Commit

---
+++ ZKRevealingTableViewCell/ZKAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  ZKAppDelegate.h
+//  ZKRevealingTableViewCell
+//
+//  Created by Alex Zielenski on 4/29/12.
+//  Copyright (c) 2012 Alex Zielenski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ZKViewController;
+
+@interface ZKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ZKViewController *viewController;
+
+@end
---
+++ ZKRevealingTableViewCell/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ZKRevealingTableViewCell
+//
+//  Created by Alex Zielenski on 4/29/12.
+//  Copyright (c) 2012 Alex Zielenski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZKAppDelegate class]));
+	}
+}
